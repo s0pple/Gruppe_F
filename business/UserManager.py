260d0f3b27b4f1222f5afdb26e726760d5d83817
Robin Jannes Haag <160750833+RobinJHaag@users.noreply.github.com>
2024-05-06 10:58:13 +0200

    UserManager Debugged.

--- business/UserManager.py
+++ business/UserManager.py
@@ -1,27 +1,24 @@
 # include all user-related functions here
 # login, register, authenticate
+###
+from sqlalchemy.testing.pickleable import User
 
 from data_access.data_base import Session, Engine
-from data_access.models import RegisteredGuest
 
 
 class UserManager:
     @staticmethod
     def register(engine: Engine, username: str, password: str):
         with Session(engine) as session:
-            new_user = RegisteredGuest(username=username, password=password)
+            new_user = User(username=username, password=password)
             session.add(new_user)
             session.commit()
 
     @staticmethod
     def login(engine: Engine, username: str, password: str):
         with Session(engine) as session:
-            user = session.query(RegisteredGuest).filter_by(username=username).first()
+            user = session.query(User).filter_by(username=username).first()
             if user and user.password == password:
                 return True
             else:
                 return False
-
-    @staticmethod
-    def authenticate(engine: Engine, username: str, password: str):
-        return UserManager.login(engine, username, password)
