41738afffdbbd90caefc709a1aea08d8f9ae7ae5
Robin Jannes Haag <160750833+RobinJHaag@users.noreply.github.com>
2024-05-20 14:21:33 +0200

    Login 1.0
    Added Login Functionality

--- business/UserManager.py
+++ business/UserManager.py
@@ -1,6 +1,8 @@
 # include all user-related functions here
 # login, register, authenticate
 ###
+from sqlalchemy.testing.pickleable import User
+
 from data_access.data_base import get_db_connection
 
 
@@ -11,3 +13,10 @@ class UserManager:
             new_user = User(username=username, password=password)
             session.add(new_user)
             session.commit()
+
+    def login(self, database_path, username, password):
+        session = get_db_connection(database_path)
+        user = session.query(User).filter_by(username=username).first()
+        if user and user.password == password:
+            return True
+        return False
