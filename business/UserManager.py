9e9c107e37d0ff865d44c29e2c305760d1190b22
Robin Jannes Haag <160750833+RobinJHaag@users.noreply.github.com>
2024-05-26 11:37:07 +0200

    Register User, new Version
    danke vaagi

--- business/UserManager.py
+++ business/UserManager.py
@@ -1,18 +1,48 @@
-# include all user-related functions here
-# login, register, authenticate
-###
-from sqlalchemy.testing.pickleable import User
-from data_models.models import Login as User
-from data_access.data_base import get_db_connection
+# imports
+from pathlib import Path
+from sqlalchemy import create_engine
+from sqlalchemy import select
+from sqlalchemy.orm import sessionmaker
+from sqlalchemy.orm.scoping import scoped_session
+from data_access.data_base import init_db
+from data_models.models import *
+
+# structure for SQAlchemy
+if __name__ == '__main__':
+    database_file = "./data/hotel_reservation.db"
+
+    database_path = Path(database_file)
+    if not database_path.parent.exists():
+        database_path.parent.mkdir()
+
+    # Tells SQAlchemy how to connect to the db.
+    engine = create_engine(f'sqlite:///{database_path}', echo=False)
+
+    Base.metadata.drop_all(engine)
+    Base.metadata.create_all(engine)
+    init_db(database_file, generate_example_data=True)
+
+    # Let's create a session to connect to the db.
+    session = scoped_session(sessionmaker(bind=engine))
+
+# creates a session to connect to the db.
+session = scoped_session(sessionmaker(bind=engine))
 
 
-class UserManager:
+def register_user():
+    login = [Login(username=input("Username: "), password=input("Password: "), role_id=int(input("Role: ")))]
+    for login in login:
+        session.add(login)
 
-    def register(self, database_path, username, password):
-        session = get_db_connection(database_path)
-        new_user = User(username=username, password=password, role_id=1)
-        print(f'User {username} registered successfully')
-        session.add(new_user)
-        session.commit()
+    session.commit()
+    print("login has been added to the database. Address ID is")
 
 
+# login()
+register_user()
+
+print("All login as objects:")
+query = select(Login)
+result = session.execute(query).scalars().all()
+for Login in result:
+    print(Login)
