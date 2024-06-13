e613338c2828fc63fc13b53e149c19d3b1ff2c49
s0pple <oliver.gwerder@gmail.com>
2024-06-13 19:20:34 +0200

    1.2 (ui.SearchMenu export functions to buisness.SearchManager)

--- business/SearchManager.py
+++ business/SearchManager.py
@@ -5,6 +5,7 @@ from business.BaseManager import BaseManager
 from data_models.models import *
 from datetime import datetime
 from business.ValidationManager import ValidationManager
+from console.console_base import Console
 
 class SearchManager(BaseManager):
     def __init__(self) -> None:
@@ -20,6 +21,11 @@ class SearchManager(BaseManager):
     def get_all_hotels(self) -> List[Hotel]:
         query = select(Hotel)
         return self.select_all(query)
+
+    def get_hotel_name_by_id(self, hotel_id):
+        query = select(Hotel.name).where(Hotel.id == hotel_id)
+        result = self._session.execute(query).scalar_one()
+        return result
 
     #
     # def get_hotels_by_name(self, name: str) -> List[Hotel]:
@@ -153,13 +159,8 @@ class SearchManager(BaseManager):
                                       price=None, description=None, start_date=None, end_date=None) -> List[Room]:
         query = select(Room)
 
-        # if not any([hotel_id, number, type, max_guests, amenities, price, description]):
-        #     return self.get_all_rooms_by_hotel_id(hotel_id)
-
-        # Add filters based on the provided criteria
         if hotel_id:
             query = query.where(Room.hotel_id == hotel_id)
-
         if number:
             query = query.where(Room.number == number)
         if type:
@@ -173,17 +174,14 @@ class SearchManager(BaseManager):
         if description:
             query = query.where(Room.description == description)
 
-        # Fetch start and end dates from ValidationManager
         start_date = self.__validation_manager.input_start_date()
         if start_date is not None:
             end_date = self.__validation_manager.input_end_date(start_date)
         else:
             end_date = None
 
-        # Filter by room availability within the given date range
         if start_date and end_date:
             br = aliased(Booking)
-
             booking_subquery = (
                 select(br.room_hotel_id, br.room_number)
                 .where(
@@ -208,11 +206,139 @@ class SearchManager(BaseManager):
         all_rooms = result.fetchall()
         return self.select_all(query), all_rooms
 
-    def get_hotel_name_by_id(self, hotel_id):
-        query = select(Hotel.name).where(Hotel.id == hotel_id)
-        result = self._session.execute(query).scalar_one()
-        return result
+    def display_all_rooms(self, hotel_id):
+        query, rooms = self.get_desired_rooms_by_hotel_id(hotel_id)
+        if not rooms:
+            print("No rooms found.")
+        else:
+            print("\nAvailable rooms:")
+            for index, room in enumerate(rooms, start=1):
+                room = room[0]
+                hotel_name = self.get_hotel_name_by_id(room.hotel_id)  # Retrieve the hotel name
+                room_info = (f"{index}. \033[4m[{hotel_name}]\033[0m\n"
+                             f"    Room Number: {room.number}\n"
+                             f"    Type: {room.type}\n"
+                             f"    Price per Night: {room.price}\n")
+                print(room_info)
+                print("-" * 80)
 
+            try:
+                choice = int(input("Enter the number you want to select: "))
+                if 1 <= choice <= len(rooms):
+                    selected_room = rooms[choice - 1][0]
+                    print(f"You selected: room number: {selected_room.number}")
+                else:
+                    print("Invalid selection. Please try again.")
+            except ValueError:
+                print("Invalid input. Please enter a valid number.")
+            return self
+
+    def search_rooms(self, hotel_id):
+        print("Select the room type you want to search for:")
+        print("1. Single Room")
+        print("2. Double Room")
+        print("3. Family Room")
+        print("4. Suite")
+        room_type_choice = input("Enter your choice (1-4 / Enter): ")
+
+        room_type_dict = {
+            "1": "single room",
+            "2": "double room",
+            "3": "family room",
+            "4": "suite",
+            "": "all",  # Default to 'all' if no choice is made
+        }
+
+        room_type = room_type_dict.get(room_type_choice, "all")
+        if room_type == "all":
+            room_type = None  # Treat 'all' as no specific room type
+
+        max_guests = input("Enter the maximum number of guests you want to search for or press Enter for all: ")
+        max_guests = int(max_guests) if max_guests.isdigit() and 1 <= int(max_guests) <= 4 else None
+
+        price = input("Enter the price per night you want to search for or press Enter for all: ")
+        price = int(price) if price.isdigit() and int(price) > 0 else None
+
+        start_date = self.__validation_manager.input_start_date()  # Replace get_start_date with input_start_date
+        end_date = self.__validation_manager.input_end_date(start_date) if start_date else None
+
+        # If all inputs are skipped, display all rooms
+        if not any([room_type, max_guests, price, start_date, end_date]):
+            return self.display_all_rooms(hotel_id)
+
+        query, rooms = self.get_desired_rooms_by_hotel_id(
+            hotel_id, type=room_type, max_guests=max_guests,
+            price=price, start_date=start_date, end_date=end_date
+        )
+
+        if not rooms:
+            print("No rooms found matching the criteria.")
+        else:
+            print("\nAvailable rooms:")
+            for index, room in enumerate(rooms, start=1):
+                try:
+                    hotel_name = self.get_hotel_name_by_id(room.Room.hotel_id)
+                    room_info = (f"{index}. \033[4m{hotel_name}\033[0m\n"
+                                 f"    Room Number: {room.Room.number}\n"
+                                 f"    Type: {room.Room.type}\n"
+                                 f"    Price per Night: {room.Room.price}\n")
+                    print(room_info)
+                    print("-" * 80)
+                except AttributeError as e:
+                    print(f"Error: {e}. Room attributes: {room._mapping}")
+
+            try:
+                choice = int(input("Enter the number you want to select: "))
+                if 1 <= choice <= len(rooms):
+                    selected_room = rooms[choice - 1].Room
+                    Console.format_text(f"You selected: room number: {selected_room.number}")
+
+                    # Ask if the user has a login
+                    has_login = input("Do you have a login? (yes/no): ")
+                    if has_login.lower() == 'yes':
+                        # If the user has a login, retrieve the Guest instance associated with that login
+                        username = input("Enter your username: ")
+                        password = input("Enter your password: ")
+                        guest = self._session.query(Guest).join(Login).filter(Login.username == username,
+                                                                              Login.password == password).first()
+                    else:
+                        # If the user doesn't have a login, ask for their details and create a new Guest instance
+                        firstname = input("Enter your first name: ")
+                        lastname = input("Enter your last name: ")
+                        email = input("Enter your email: ")
+                        street = input("Enter your street and house number: ")
+                        zip = input("Enter your zip code: ")
+                        city = input("Enter your city: ")
+
+                        address = Address(street=street, zip=zip, city=city)
+                        self._session.add(address)
+                        self._session.commit()
+
+                        guest = Guest(firstname=firstname, lastname=lastname, email=email, address_id=address.id)
+                        self._session.add(guest)
+                        self._session.commit()
+
+                    booking_manager = BookingManager()  # Assuming you have a BookingManager class
+                    hotel_name = self.get_hotel_name_by_id(selected_room.hotel_id)
+                    start_date = self.__validation_manager.input_start_date()
+                    if start_date is not None:
+                        end_date = self.__validation_manager.input_end_date(start_date)
+                    else:
+                        end_date = None
+
+                    # Ask the user for the number of guests
+                    number_of_guests = input("Enter the number of guests: ")
+                    number_of_guests = int(number_of_guests) if number_of_guests.isdigit() else None
+
+                    booking_manager.add_booking(hotel_name=hotel_name, start_date=start_date,
+                                                end_date=end_date, hotel_id=selected_room.hotel_id,
+                                                room_id=selected_room.number, guest_id=guest.id,
+                                                number_of_guests=number_of_guests)
+                else:
+                    Console.format_text("Invalid selection. Please try again.")
+            except ValueError:
+                Console.format_text("Invalid input. Please enter a valid number.")
+            return self
 
 if __name__ == '__main__':
     # This is only for testing without Application
